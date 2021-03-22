6d92b4a13a20f77bd627d6532d6176242fb0f920
atsalibram <atsalibaram@gmail.com>
2021-03-22 13:22:15 +0300

    added location class to models

--- gallery/models.py
+++ gallery/models.py
@@ -14,3 +14,12 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+class Location(models.Model):
+    location=models.CharField(max_length=60)
+
+    def save_location(self):
+        self.save()
+
+    def __str__(self):
+        return self.location 
