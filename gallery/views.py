87369b00347011b5b8906af4286796251382da6b
atsalibram <atsalibaram@gmail.com>
2021-03-22 12:29:40 +0300

    added urls.py

--- gallery/views.py
+++ gallery/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.http  import HttpResponse
 
 # Create your views here.
+
+def mygallery(request):
+    date = dt.date.today()
+    photos = Image.get_all()
+    return render(request, 'all-pics/home.html', {"date": date, "photos":photos})
