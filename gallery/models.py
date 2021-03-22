12249b38aba1382d16d196b8b787d44ab61fa259
atsalibram <atsalibaram@gmail.com>
2021-03-22 13:21:16 +0300

    added category class

--- gallery/models.py
+++ gallery/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+class Category(models.Model):
+    name=models.CharField(max_length=60)
+
+    def save_category(self):
+        self.save()
+
+    @classmethod
+    def search_by_name(cls,search_term):
+        pgallery = cls.objects.filter(name__icontains=search_term)
+        return pgallery
+
+    def __str__(self):
+        return self.name
