2ec01934f715496e949e8de480df2e7ae4a14d9d
atsalibram <atsalibaram@gmail.com>
2021-03-22 13:26:09 +0300

    update url.py

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,11 +1,13 @@
-from django.conf.urls import url
-from . import views
-import datetime as dt
 from django.conf import settings
+from django.conf.urls import url
 from django.conf.urls.static import static
+from . import views
 
 urlpatterns=[
-    
-    url(r'^$',views.mygallery,name='pics'),
+    url('^$',views.index,name='index.html'),
     url(r'^search/', views.search_results, name='search_results'),
+    url(r'^image/(\d+)',views.image,name ='image')
+
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
