9fac4dd30165be3fcc26b1ea05cf4d2772d67b98
atsalibram <atsalibaram@gmail.com>
2021-03-22 13:19:24 +0300

    added url conf

--- gallery/admin.py
+++ gallery/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Image,Category,Location
+
+class ImageAdmin(admin.ModelAdmin):
+    filter_horizontal=('location',)
 
 # Register your models here.
+admin.site.register(Image)
+admin.site.register(Category)
+admin.site.register(Location)
