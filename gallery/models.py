3e16c89a8bdede1748675d5fafb5f5767c1009ec
atsalibram <atsalibaram@gmail.com>
2021-03-22 13:23:57 +0300

    added imge class to models

--- gallery/models.py
+++ gallery/models.py
@@ -23,3 +23,15 @@ class Location(models.Model):
 
     def __str__(self):
         return self.location 
+
+        class Image(models.Model):
+    image=models.ImageField(upload_to='mygallery/', blank=True)
+    image_link=models.TextField(blank=True)
+    image_name=models.TextField(max_length=60 , blank=True)
+    details=models.TextField(max_length=60, blank=60)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    category = models.ManyToManyField(Category, blank=True)
+    location=models.ForeignKey(Location, blank=True, null=True)
+
+    def save_image(self):
+        self.save()
